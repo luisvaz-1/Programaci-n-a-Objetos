106786f3f7513126e912e70457a8003b79500770
luisvaz-1 <153474639+luisvaz-1@users.noreply.github.com>
2024-11-15 21:25:11 -0600

    Create AsistenteClase.java

---
+++ src/main/my/com/vmbl/mi/com/AsistenteClase.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package my.com.vmbl.poo.polimorfismo;
+
+/**
+ *
+ * @author victor-briseno
+ */
+public abstract class AsistenteClase {
+    public abstract void presentacion();
+    public abstract void examen();
+    public abstract void proyecto();
+    public abstract String ToString();
+}
